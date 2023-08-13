7efc0dac86b77010d70c9354a20d679dff8d5384
Zainab Al-Masoodi <zainab_almasoodi@yahoo.com>
2023-08-13 14:55:52 -0400

    final project

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
+import Currency from './components/Currency';
 const App = () => {
     return (
         <AppProvider>
@@ -31,7 +32,10 @@ const App = () => {
                         {/* Add ExpenseTotal component here under */} 
                     <div className='col-sm'>
                         <ExpenseTotal />
-                    </div>       
+                    </div>  
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>     
                        
                     </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,59 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+const budgetMax = 20000;
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, totalExpenses,currency, dispatch } = useContext(AppContext);
+
+  const onChangeBudgetHandler = (event) => {
+    const enteredValue = Number(event.target.value);
+
+    
+    if (enteredValue < totalExpenses) {
+      alert(
+        "The value of the buget can't be lower than the expenses value " +
+          currency +
+          totalExpenses
+      );
+
+      return;
+    }
+     else {
+      if (enteredValue > budgetMax) {
+        alert('Please enter a value less that ' + budgetMax);
+        return;
+      }
+
+      dispatch({
+        type: 'SET_BUDGET',
+        payload: enteredValue,
+      });
+    }
+  };
+
+  return (
+    <div
+      className="alert alert-secondary"
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+    >
+      <div>
+        <label htmlFor="budget">Budget:&nbsp;</label>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span>{currency}</span>
+        <input
+          required="required"
+          placeholder='enter your budget'
+          type="number"
+          id="budget"
+          value={budget}
+          step="10"
+          onChange={onChangeBudgetHandler}
+        ></input>
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
